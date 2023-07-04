28c0e6b72c183cc7a81c66580ac0be202791ef8f
Regan Tan <regantanyk@gmailcom>
2023-07-04 20:14:50 +0800

    changes to while true loop in main.py

--- main.py
+++ main.py
@@ -78,4 +78,27 @@ while true:
     shape_type = input("What do you like to draw? Enter quit to quit. ")
     # Ask for rectangle data and create rectangle if user entered 'rectangle'
     if shape_type.lower() == 'rectangle':
-        rec_x = int(input(""))
+        rec_x = int(input("Enter x of rectangle: "))
+        rec_y = int(input("Enter y of rectangle: "))
+        rec_width = int(input("Enter width of rectangle: "))
+        rec_height = int(input("Enter height of rectangle: "))
+        red = int(input("How much red should the rectangle have? "))
+        green = int(input("How much green? "))
+        blue = int(input("How much blue? "))
+
+        # Create the rectangle
+        r1 = Rectangle(x=rec_x, y=rec_y, height=rec_height, width=rec_width, color=(red, green, blue))
+        r1.draw(canvas)
+
+    # Ask for square data and create square if user entered 'square'
+    if shape_type.lower() == 'square':
+        sqr_x = int(input("Enter x of square: "))
+        sqr_y = int(input("Enter y of square: "))
+        sqr_side = int(input("Enter the length of a side of the square: "))
+        red = int(input("How much red should the square have? "))
+        green = int(input("How much green? "))
+        blue = int(input("How much blue? "))
+
+        # Create the square
+        r1 = Square(x=sqr_x, y=sqr_y, side=sqr_side, color=(red, green, blue))
+        r1.draw(canvas)
